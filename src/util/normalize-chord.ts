5f5b712c98d4840fbab8282453a184011a1ac496
SmallhillCZ <github@smallhill.cz>
2020-12-04 16:30:09 +0000

    fix normalizing of sus chords

--- src/util/normalize-chord.ts
+++ src/util/normalize-chord.ts
@@ -1,9 +1,13 @@
 export function normalizeChord(chord: string): string {
   return [
-    
+
     chord.substr(0, 1).toUpperCase(),
-    
-    chord.substr(1).replace(/^es|s|b/, "♭").replace(/^is|#/, "♯")
-    
+
+    chord.substr(1).replace(/^(?:es|s(?!us)|b)/, "♭").replace(/^is|#/, "♯")
   ].join("");
+}
+
+function superscriptNumber(number: string) {
+  const numbers = "⁰¹²³⁴⁵⁶⁷⁸⁹";
+  return number.split("").map(char => numbers.charAt(Number(char)));
 }
